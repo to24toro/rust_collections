31ccbe9cdac7e31f8e6f964862e6854b25aac8e6
to24toro <to24toro@gmail.com>
2022-07-30 09:30:56 +0900

    First implementation of grrs

--- practice/grrs/src/main.rs
+++ practice/grrs/src/main.rs
@@ -11,4 +11,11 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }

817afab00cad111ebfebee54270e2f3510a1c83d
to24toro <to24toro@gmail.com>
2022-07-30 09:41:12 +0900

    Nicer error reporting

--- practice/grrs/src/main.rs
+++ practice/grrs/src/main.rs
@@ -1,21 +1,30 @@
-#![allow(unused)]
+// #![allow(unused)]
 
-use clap::Parser;
+// use clap::Parser;
 
-#[derive(Parser)]
-struct Cli {
-    pattern: String,
-    #[clap(parse(from_os_str))]
-    path: std::path::PathBuf,
-}
+// #[derive(Parser)]
+// struct Cli {
+//     pattern: String,
+//     #[clap(parse(from_os_str))]
+//     path: std::path::PathBuf,
+// }
 
-fn main() {
-    let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+// fn main() {
+//     let args = Cli::parse();
+//     let content = std::fs::read_to_string(&args.path).expect("could not read file");
 
-    for line in content.lines() {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
-    }
+//     for line in content.lines() {
+//         if line.contains(&args.pattern) {
+//             println!("{}", line);
+//         }
+//     }
+// }
+
+use anyhow::{Context, Result};
+
+fn main() -> Result<()> {
+    let path = "text.txt";
+    let content = std::fs::read_to_string(path).with_context(|| format!("could not read file `{}`", path))?;
+    println!("file content: {}", content);
+    Ok(())
 }

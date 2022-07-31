46ee0a8034a7b3ba4cd84c3ab8699c35ea5b2271
to24toro <to24toro@gmail.com>
2022-07-31 11:29:18 +0900

    ex_12

---
+++ practice/minigrep/src/main.rs
@@ -0,0 +1,26 @@
+extern crate minigrep;
+use std::env;
+use std::process;
+
+use minigrep::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        // 引数解析時に問題
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+
+
+
+
+
